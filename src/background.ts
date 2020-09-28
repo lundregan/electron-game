44672bad72ab114c8497cdf833f33e79ad04ffd0
lundregan <lundregan.ethan@gmail.com>
2020-09-28 15:59:12 +0100

    Removed boilerplate code

--- src/background.ts
+++ src/background.ts
@@ -2,6 +2,7 @@
 
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+//@ts-ignore
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -17,8 +18,8 @@ protocol.registerSchemesAsPrivileged([
 function createWindow() {
   // Create the browser window.
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
---
+++ src/views/Game.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="game">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
