c45e8dd86a4492af0544258d98970b7522cf3404
lundregan <lundregan.ethan@gmail.com>
2020-09-28 18:50:00 +0100

    Generated tileGrid for handling multiple grid sizes

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="game">
+  <section class="game container">
     <h1>Playing Game</h1>
 
-    <div class="tiles" style='display: flex; flex-direction: column;'>
-      <div class="rows" v-for='x in tiles.width' :key='x.id' style='display: flex;'>
-        <div 
-          class="tile"
-          v-for='y in tiles.height'
-          :key='y.id' @click='test(x, y)'
-          :class='{"tile-active" : tileGrid[x-1][y-1].active, "tile-inactive" : !tileGrid[x-1][y-1].active}'
-        ></div>
-      </div>
+    <div class="columns">
+        <div class="column"></div>
+        <div class="column">
+        <div class="gtiles" style='display: flex; flex-direction: column;'>
+          <div class="rows" v-for='x in tileGrid.length' :key='x.id' style='display: flex;'>
+            <div 
+              class="gtile"
+              v-for='y in tileGrid.length'
+              :key='y.id' @click='test(x, y)'
+              :class='{"tile-active" : tileGrid[x-1][y-1].active, "tile-inactive" : !tileGrid[x-1][y-1].active}'
+            ></div>
+          </div>
+        </div>
+        </div>
+        <div class="column"></div>
     </div>
-  </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
-.tile {
-  padding: 50px;
+.gtile {
+  padding: 60px;
 
   margin: 1px;
 }
@@ -47,17 +53,28 @@ export default {
   name: 'Home',
   components: {
   },
+  mounted () {
+    const generatedTileGrid = [];
+
+    for(let i = 0; i < this.tileGridSize; i++){
+      const columnArray = [];
+
+      for(let j = 0; j < this.tileGridSize; j++){
+        columnArray.push(
+          new Tile('default', false)
+        );
+      }
+
+      generatedTileGrid.push(columnArray);
+    }
+
+    console.log(generatedTileGrid)
+    this.tileGrid = generatedTileGrid;
+  },
   data () {
     return {
-      tileGrid: [
-        [new Tile('default', false), new Tile('default', false), new Tile('default', false)],
-        [new Tile('default', false), new Tile('default', false), new Tile('default', false)],
-        [new Tile('special', false), new Tile('default', false), new Tile('default', false)]
-      ],
-      tiles: {
-        height: 3,
-        width: 3
-      }
+      tileGridSize: 10,
+      tileGrid: []
     }
   },
   methods: {
