a0735c416fdd0dc70463c8d2fe63cc81bff51016
lundregan <lundregan.ethan@gmail.com>
2020-10-01 20:08:28 +0100

    disabled menu options for now

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,12 +3,48 @@
       <b-button class='is-primary' @click='$router.go(-1)'>Back to game</b-button>
       <hr>
       <p class='title'>Settings</p>
+      
+      <div class="columns">
+        <div class="column">
+          <b-field label="Theme">
+            <b-select :placeholder="themes[0]" disabled>
+              <option
+                v-for="theme in themes"
+                :value="theme.id"
+                :key="theme.id"
+              >
+                {{ theme }}
+              </option>
+            </b-select>
+          </b-field>
+          <b-field label="Color Blind Mode">
+            <b-select :placeholder="colorBlindModes[0]" disabled>
+              <option
+                v-for="colorBlindMode in colorBlindModes"
+                :value="colorBlindMode.id"
+                :key="colorBlindMode.id"
+              >
+                {{ colorBlindMode }}
+              </option>
+            </b-select>
+          </b-field>
+        </div>
+        </div>
   </div>
 </template>
 
 <script>
 export default {
-
+data () {
+  return {
+    themes: [
+      'Default Light'
+    ],
+    colorBlindModes: [
+      'Off'
+    ]
+  }
+}
 }
 </script>
 
