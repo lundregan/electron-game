6d279759847b9e988a3d84a5d73603b61d887f92
lundregan <lundregan.ethan@gmail.com>
2020-10-23 15:09:09 +0100

    simplified tileboard

--- src/data/TileBoard.ts
+++ src/data/TileBoard.ts
@@ -2,12 +2,8 @@ import store from '../store/index'
 
 import { Tile } from './Tiles/Tile'
 import { Default } from './Tiles/Default'
-import { SingleDirection } from './Tiles/SingleDirection'
-import { Disabled } from './Tiles/Disabled'
-import { Invisible } from './Tiles/Invisible'
-import { SingleLineDirection } from './Tiles/SingleLineDirection'
 import { Tiles } from './Tiles/Tiles'
-import { Bomb } from './Tiles/Bomb'
+
 
 export class TileBoard {
     size: number
@@ -20,6 +16,7 @@ export class TileBoard {
       this.generateBoard()
       this.tiles = new Tiles()
     }
+
     generateBoard() {
       const generatedTileGrid = [];
   
@@ -37,6 +34,7 @@ export class TileBoard {
   
       this.board = generatedTileGrid;
     }
+
     ToggleTileLater(cords: Array<number>){
         setTimeout(() => {
             store.dispatch('toggleTile', cords)
@@ -44,11 +42,13 @@ export class TileBoard {
             100
         );
     }
+
     toggleTile(x: number, y: number) {
         if(!this.outOfBounds(x, y)){
             this.board[x][y].toggle()
         }
     }
+
     outOfBounds(x: number, y: number){
         if(
             x < 0 ||
@@ -60,33 +60,6 @@ export class TileBoard {
         }
 
         return false
-    }
-    loadLevel(level: any) {
-        const loadedTileGrid = []
-  
-        for(let i = 0; i < this.size; i++){
-            const columnArray = [];
-    
-            for(let j = 0; j < this.size; j++){
-                const newTile ={ 
-                    x: level.board[i][j].x,
-                    y: level.board[i][j].y,
-                    type: level.board[i][j].type,
-                    active: level.board[i][j].active
-                }
-
-                const newTileObject = this.getTileClassObject(newTile.type, newTile.x, newTile.y)
-
-                columnArray.push(
-                    //new Tile(newTile.type, newTile.active, newTile.x, newTile.y)
-                    newTileObject
-                );
-            }
-    
-            loadedTileGrid.push(columnArray);
-        }
-    
-        this.board = loadedTileGrid;
     }
 
     loadJson(levelData: any){
@@ -117,34 +90,7 @@ export class TileBoard {
     }
 
     setTileType(x: number, y: number, type: string){
-        this.board[x][y] = this.getTileClassObject(type, x, y)
+        this.board[x][y] = this.tiles.newTile(type, x, y, false, null)
     }
-    getTileClassObject(type: string, x: number, y: number){
-        switch(type){
-            case 'single-left':
-                return new SingleDirection(false, x, y, 'left')
-            case 'single-right':
-                return new SingleDirection(false, x, y, 'right')
-            case 'single-up':
-                return new SingleDirection(false, x, y, 'up')
-            case 'single-down':
-                return new SingleDirection(false, x, y, 'down')
-            case 'line-left':
-                return new SingleLineDirection(false, x, y, 'left')    
-            case 'line-right':
-                return new SingleLineDirection(false, x, y, 'right')
-            case 'line-up':
-                return new SingleLineDirection(false, x, y, 'up')    
-            case 'line-down':
-                return new SingleLineDirection(false, x, y, 'down')  
-            case 'Disabled':
-                return new Disabled(x, y)
-            case 'Invisible':
-                return new Invisible(x, y)
-            case 'Bomb':
-                return new Bomb(false, x, y)
-            default:
-                return new Default(false, x, y)
-        }
-    }
+    
 }
